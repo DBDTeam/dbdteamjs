6571fad08342dff9992c97b753a0aa5f0639d5fc
ImSirSpace <joaalesand@gmail.com>
2024-03-12 19:59:07 -0300

    Added field 'articles' if the message has an URL

--- structures/Actions/Events/MESSAGE_UPDATE.js
+++ structures/Actions/Events/MESSAGE_UPDATE.js
@@ -1,8 +1,13 @@
 const { Message } = require("../../../structures/Message.js");
 
 module.exports = async(client, d, id) => {
-    var msg = await new Message(d, client)
-    
+    if(d.embeds){
+        var msg = await client.channels.cache.get(d.channel_id).messages.fetch(d.id)
+        msg.articles = d.embeds
+    } else {
+        var msg = await new Message(d, client)
+    }
+
     msg.channel.messages.cache.set(d.id, msg)
 
     client.emit("messageUpdate", msg, id)
