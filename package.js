70fd3af0d34bb9cbcf0d731b9984b1730959980f
ImSirSpace <joaalesand@gmail.com>
2024-03-12 19:35:24 -0300

    Added all (or well, most) exports of important things

--- package.js
+++ package.js
@@ -2,28 +2,51 @@
 const { Client } = require("./structures/Client/Client");
 const { ClientPresence } = require("./structures/Client/ClientPresence");
 const { ClientUser } = require("./structures/Client/ClientUser");
-const { Sharding } = require("./structures/Sharding");
+const { ShardManager, Shard } = require("./structures/Sharding");
 const { Base } = require("./structures/Base");
+const { User } = require("./structures/User");
+const { Member } = require("./structures/Member");
+const { Guild } = require("./structures/Guild");
+const { Channel } = require("./structures/DefaultChannel");
+const { CategoryChannel } = require("./structures/CategoryChannel");
+const { ThreadChannel } = require("./structures/ThreadChannel");
+const { TextChannel } = require("./structures/TextChannel");
+const { ThreadMember } = require("./structures/ThreadMember");
+const { VoiceChannel } = require("./structures/VoiceChannel");
 
 //Types
 const { IntentBitFields } = require("./structures/Flags/Intents.js")
 const { SlashTypes, InteractionTypes, ComponentTypes, ButtonStyles } = require("./Types/Interactions.js")
-
-// Others
-const { User } = require("./structures/User");
+const { Presence } = require("./Types/PresenceInfo")
+const { ChannelTypes } = require("./Types/ChannelTypes")
 
 // Utils
 const Collection = require("./Utils/Collection");
-const { resolveImage } = require("./Utils/ImageResolver");
-const { Presence } = require("./Types/PresenceInfo.js");
+const { resolveImage } = require("./Utils/ImageResolver.js")
 
 module.exports = {
   Client,
+  ClientPresence,
+  ClientUser,
+  ShardManager,
+  Shard,
+  Base,
+  User,
+  Member,
+  Guild,
+  Channel,
+  CategoryChannel,
+  ThreadChannel,
+  TextChannel,
+  ThreadMember,
+  VoiceChannel,
   IntentBitFields,
-  Collection,
-  Presence,
   SlashTypes,
   InteractionTypes,
   ComponentTypes,
   ButtonStyles,
+  Presence,
+  ChannelTypes,
+  Collection,
+  resolveImage
 };
