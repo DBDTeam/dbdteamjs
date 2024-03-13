97e53715bf731cc95d2b96f4bf9d9bc811704e7c
ImSirSpace <joaalesand@gmail.com>
2024-03-13 12:07:39 -0300

    Avoiding errors

--- structures/Actions/Events/MESSAGE_CREATE.js
+++ structures/Actions/Events/MESSAGE_CREATE.js
@@ -2,8 +2,7 @@ const { Message } = require("../../../structures/Message.js");
 
 module.exports = async(client, d, id) => {
     var msg = await new Message(d, client)
-    
-    msg.channel.messages.cache.set(d.id, msg)
+    msg?.channel?.messages?.cache?.set(d.id, msg)
 
     client.emit("messageCreate", msg, id)
 }
