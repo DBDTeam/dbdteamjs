4df42dbd1618973057ef79f960d1cf662c9ab1b3
ImSirSpace <joaalesand@gmail.com>
2024-03-23 11:04:57 -0300

    Fixed the MessageUpdate error

--- structures/Actions/Events/MESSAGE_UPDATE.js
+++ structures/Actions/Events/MESSAGE_UPDATE.js
@@ -1,19 +1,19 @@
 const { Message } = require("../../../structures/Message.js");
 
-module.exports = async(client, d, id) => {
-    if(d.embeds){
-        var msg = client.channels.cache.get(d.channel_id).messages.cache.get(d.id)
-        
-        if(!msg) {
-            msg = await client.channels.cache.get(d.channel_id).messages.fetch(d.id)
-        }
+module.exports = async (client, d, id) => {
+    var msg;
 
-        msg.articles = d.embeds.filter(x => x.type === "article")
+    const channel = client.channels.cache.get(d.channel_id);
+    if (!channel) return;
+
+    if (d.embeds) {
+        msg = channel.messages.cache.get(d.id);
+        if (!msg) msg = await channel.messages.fetch(d.id);
+        msg.articles = d.embeds.filter(x => x.type === "article");
     } else {
-        var msg = await new Message(d, client)
+        msg = d.author ? await new Message(d, client) : channel.messages.cache.get(d.id) || await channel.messages.fetch(d.id);
     }
 
-    msg.channel.messages.cache.set(d.id, msg)
-
-    client.emit("messageUpdate", msg, id)
-}
+    if (msg) msg.channel.messages.cache.set(d.id, msg);
+    client.emit("messageUpdate", msg, id);
+};
