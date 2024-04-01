41068167df877bf538261e0dea9407342830a443
ImSirSpace <joaalesand@gmail.com>
2024-03-31 22:01:40 -0300

    Adding some things (idk why i'm doing this xd)

--- src/structures/Actions/Event.ts
+++ src/structures/Actions/Event.ts
@@ -1,6 +1,13 @@
 import { type Client } from "../../client/Client";
+import { typeChannel } from "../../utils/utils";
 import { Member } from "../Member";
 import { Message } from "../Message";
+import { User } from "../User";
+
+export enum EventNames {
+  "MessageCreate" = "messageCreate",
+  "InteractionCreate" = "interactionCreate"
+}
 
 export abstract class Event {
   constructor(public client: Client) {
@@ -15,14 +22,37 @@ export abstract class Event {
     message?.channel?.messages?.cache?.set(data.id, message);
 
     if (data.member && !message?.guild?.members.cache.get(data.author.id)) {
+      const member = new Member(
+        { ...data.member, id: data.author.id },
+        message.guild,
+        this.client
+      )
       message?.guild?.members?.cache.set(
         data.author.id,
-        new Member(
-          { ...data.member, id: data.author.id },
-          message.guild,
-          this.client
-        )
+        member
       );
+
+      this.client.users.cache.set(data.author.id, member.user)
     }
+
+    return message
+  }
+
+  getChannel(data: any) {
+    const channel = typeChannel(data, this.client)
+
+    channel.guild?.channels?.cache?.set(channel.id, channel)
+
+    this.client?.channels?.cache?.set(channel.id, channel)
+
+    return channel
+  }
+
+  getUser(data: any) {
+    const user = new User(data, this.client)
+
+    this.client?.users?.cache?.set(user.id, user)
+
+    return user
   }
 }
--- src/structures/Actions/Events/MESSAGE_CREATE.ts
+++ src/structures/Actions/Events/MESSAGE_CREATE.ts
@@ -1,29 +1,13 @@
 import { GatewayMessageCreateDispatchData } from "discord-api-types/v10";
 
-import { Member } from "../../Member.js";
-import { Message } from "../../Message.js";
-
-import { Event } from "../Event";
+import { Event, EventNames } from "../Event";
 
 export default class MessageCreate extends Event {
   handle(data: GatewayMessageCreateDispatchData) {
     const client = this.client;
-    const message = new Message(data, client);
+    const message = this.getMessage(data)
 
-    message?.channel?.messages?.cache?.set(data.id, message);
-
-    if (data.member && !message?.guild?.members.cache.get(data.author.id)) {
-      message?.guild?.members?.cache.set(
-        data.author.id,
-        new Member(
-          { ...data.member, id: data.author.id },
-          message.guild,
-          client
-        )
-      );
-    }
-
-    client.emit("messageCreate", message);
+    client.emit(EventNames.MessageCreate, message);
 
     return { message };
   }
