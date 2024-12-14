cb2c100831b7731712a35bc82dac9ebee80d7d88
ImSirSpace <joaalesand@gmail.com>
2024-12-13 23:30:08 -0300

    Added user data update tracking (UserUpdate)

--- src/client/events/Events/GUILD_MEMBER_UPDATE.ts
+++ src/client/events/Events/GUILD_MEMBER_UPDATE.ts
@@ -1,15 +1,16 @@
 import { GatewayGuildMemberUpdateDispatchData } from "discord-api-types/v10";
 import { Event } from "../Event";
-import { Shard } from "../../../structures";
+import { Member, Shard } from "../../../structures";
 import { EventNames } from "../../../common";
 
 export default class GuildMemberUpdate extends Event<GatewayGuildMemberUpdateDispatchData> {
   handle(data: GatewayGuildMemberUpdateDispatchData, shard: Shard) {
     const guild = this.client.guilds.cache.get(data.guild_id);
 
-    const oldMember = guild?.members?.cache.get(data.user.id);
-    if (!oldMember) return;
+    const oldMember = guild?.members?.cache.get(data.user.id) as Member;
     const newMember = this.getMember(data, guild?.id);
+
+    this.client.users.cache.set(newMember.id, newMember.user)
 
     this.client.emit(EventNames.GuildMemberUpdate, oldMember, newMember, shard);
   }
