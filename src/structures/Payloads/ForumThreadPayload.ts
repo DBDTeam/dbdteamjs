45eaa443fd28abcf8903ade5246efd153062309c
ImSirSpace <joaalesand@gmail.com>
2024-12-16 00:10:18 -0300

    Created a payload for Forum Threads

---
+++ src/structures/Payloads/ForumThreadPayload.ts
@@ -0,0 +1,27 @@
+import { RESTPostAPIGuildForumThreadsJSONBody, Snowflake } from "discord-api-types/v10";
+import { MessagePayloadFileData } from "../../interfaces/message/MessagePayload";
+import { MessageBodyRequest, Nullable } from "../../common";
+
+export class ForumThreadPayload {
+  name: string;
+  auto_archieve_duration: Nullable<number>;
+  rate_limit_per_user: Nullable<number>;
+  message: MessageBodyRequest;
+  applied_tags: Nullable<Snowflake[]>;
+  files: Nullable<MessagePayloadFileData[]>;
+  constructor(
+    object: RESTPostAPIGuildForumThreadsJSONBody,
+    files?: MessagePayloadFileData[]
+  ) {
+    this.name = object.name;
+    this.auto_archieve_duration = object.auto_archive_duration ?? 0;
+    this.rate_limit_per_user = object.rate_limit_per_user ?? 0;
+    this.message = object.message;
+    this.files = files || [];
+    this.applied_tags = object.applied_tags || [];
+  }
+
+  payload() {
+    return this
+  }
+}
